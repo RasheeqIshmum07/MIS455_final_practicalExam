5121729abbab6dd0b0c10a55dac126dc48bc98c6
rasheeq95 <rasheeqishmum95@gmail.com>
2023-12-13 12:13:36 +0600

    final touch

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 function getWeather() {
-      const apiKey = '4312294081b35ccd94ceffe7639aad67';
-      const city = document.getElementById('city').value;
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const apiKey = '4312294081b35ccd94ceffe7639aad67';
+    const city = document.getElementById('city').value;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-      fetch(apiUrl)
+    fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-          displayWeather(data);
+            displayWeather(data);
         })
         .catch(error => {
-          console.error('Error fetching weather data:', error);
-          document.getElementById('weather-container').innerHTML = 'Error fetching weather data.';
+            console.error('Error fetching weather data:', error);
+            document.getElementById('weather-container').innerHTML = 'Error fetching weather data.';
         });
-    }
+}
 
-    function displayWeather(data) {
-      const weatherContainer = document.getElementById('weather-container');
+function displayWeather(data) {
+    const weatherContainer = document.getElementById('weather-container');
 
-      if (data.cod === 'error 404') {
+    if (data.cod === 'error 404') {
         weatherContainer.innerHTML = 'City not found';
-      } else {
+    } else {
         const temperature = Math.round(data.main.temp - 273.15); // Convert from Kelvin to Celsius
         const description = data.weather[0].description;
 
         weatherContainer.innerHTML = `<p>Temperature: ${temperature}°C</p>
                                       <p>Description: ${description}</p>`;
-      }
     }
+}
 
 
-    
